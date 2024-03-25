b2b888769632b81961784df8bf747b92ccd58764
Barson Cath <bathcarson@gmail.com>
2024-03-24 17:21:55 -0700

    Update colors.kt

--- Compose/colors.kt
+++ Compose/colors.kt
@@ -3,22 +3,354 @@ val black = Color(0xFFFFFFFF)
 val white = Color(0xFF000000)
 
 
-val red = Color(0xFFF44336)
-val pink = Color(0xFFE91E63)
-val purple = Color(0xFF9C27B0)
-val deepPurple = Color(0xFF673AB7)
-val indigo = Color(0xFF3F51B5)
-val blue = Color(0xFF2196F3)
-val lightBlue = Color(0xFF03A9F4)
-val cyan = Color(0xFF00BCD4)
-val teal = Color(0xFF009688)
-val green = Color(0xFF4CAF50)
-val lightGreen = Color(0xFF8BC34A)
-val lime = Color(0xFFCDDC39)
-val yellow = Color(0xFFFFEB3B)
-val amber = Color(0xFFFFC107)
-val orange = Color(0xFFFF9800)
-val deepOrange = Color(0xFFFF5722)
-val brown = Color(0xFF795548)
-val grey = Color(0xFF9E9E9E)
-val blueGrey = Color(0xFF607D8B)
+// 000080 : dark_blue
+// 2a7fff : light_blue
+// 5fbcd3 : cyanish
+// 5fd3bc : tealish
+// ffdd55 : yellow
+// ff9955 : orange
+// ff80b2 : pink
+// fc3c3c : red
+val defaultPalette = listOf<Color>(
+    Color(0xFF000080),
+    Color(0xFF2a7fff),
+    Color(0xFF5fbcd3),
+    Color(0xFF5fd3bc),
+    Color(0xFFffdd55),
+    Color(0xFFff9955),
+    Color(0xFFff80b2),
+    Color(0xFFfc3c3c),
+)
+
+
+val transparent = Color(0x00000000)
+
+val white = Color(0xFFFFFFFF)
+val black = Color(0xFF000000)
+
+
+val red_50 = Color(0xFFFFEBEE)
+val red_100 = Color(0xFFFFCDD2)
+val red_200 = Color(0xFFEF9A9A)
+val red_300 = Color(0xFFE57373)
+val red_400 = Color(0xFFEF5350)
+val red_500 = Color(0xFFF44336)
+val red_600 = Color(0xFFE53935)
+val red_700 = Color(0xFFD32F2F)
+val red_800 = Color(0xFFC62828)
+val red_900 = Color(0xFFB71C1C)
+val red_A100 = Color(0xFFFF8A80)
+val red_A200 = Color(0xFFFF5252)
+val red_A400 = Color(0xFFFF1744)
+val red_A700 = Color(0xFFD50000)
+val red = red_500
+
+
+val pink_50 = Color(0xFFFCE4EC)
+val pink_100 = Color(0xFFF8BBD0)
+val pink_200 = Color(0xFFF48FB1)
+val pink_300 = Color(0xFFF06292)
+val pink_400 = Color(0xFFEC407A)
+val pink_500 = Color(0xFFE91E63)
+val pink_600 = Color(0xFFD81B60)
+val pink_700 = Color(0xFFC2185B)
+val pink_800 = Color(0xFFAD1457)
+val pink_900 = Color(0xFF880E4F)
+val pink_A100 = Color(0xFFFF80AB)
+val pink_A200 = Color(0xFFFF4081)
+val pink_A400 = Color(0xFFF50057)
+val pink_A700 = Color(0xFFC51162)
+val pink = pink_500
+
+
+val purple_50 = Color(0xFFF3E5F5)
+val purple_100 = Color(0xFFE1BEE7)
+val purple_200 = Color(0xFFCE93D8)
+val purple_300 = Color(0xFFBA68C8)
+val purple_400 = Color(0xFFAB47BC)
+val purple_500 = Color(0xFF9C27B0)
+val purple_600 = Color(0xFF8E24AA)
+val purple_700 = Color(0xFF7B1FA2)
+val purple_800 = Color(0xFF6A1B9A)
+val purple_900 = Color(0xFF4A148C)
+val purple_A100 = Color(0xFFEA80FC)
+val purple_A200 = Color(0xFFE040FB)
+val purple_A400 = Color(0xFFD500F9)
+val purple_A700 = Color(0xFFAA00FF)
+val purple = purple_500
+
+
+val deepPurple_50 = Color(0xFFEDE7F6)
+val deepPurple_100 = Color(0xFFD1C4E9)
+val deepPurple_200 = Color(0xFFB39DDB)
+val deepPurple_300 = Color(0xFF9575CD)
+val deepPurple_400 = Color(0xFF7E57C2)
+val deepPurple_500 = Color(0xFF673AB7)
+val deepPurple_600 = Color(0xFF5E35B1)
+val deepPurple_700 = Color(0xFF512DA8)
+val deepPurple_800 = Color(0xFF4527A0)
+val deepPurple_900 = Color(0xFF311B92)
+val deepPurple_A100 = Color(0xFFB388FF)
+val deepPurple_A200 = Color(0xFF7C4DFF)
+val deepPurple_A400 = Color(0xFF651FFF)
+val deepPurple_A700 = Color(0xFF6200EA)
+val deepPurple = deepPurple_500
+
+
+val indigo_50 = Color(0xFFE8EAF6)
+val indigo_100 = Color(0xFFC5CAE9)
+val indigo_200 = Color(0xFF9FA8DA)
+val indigo_300 = Color(0xFF7986CB)
+val indigo_400 = Color(0xFF5C6BC0)
+val indigo_500 = Color(0xFF3F51B5)
+val indigo_600 = Color(0xFF3949AB)
+val indigo_700 = Color(0xFF303F9F)
+val indigo_800 = Color(0xFF283593)
+val indigo_900 = Color(0xFF1A237E)
+val indigo_A100 = Color(0xFF8C9EFF)
+val indigo_A200 = Color(0xFF536DFE)
+val indigo_A400 = Color(0xFF3D5AFE)
+val indigo_A700 = Color(0xFF304FFE)
+val indigo = indigo_500
+
+
+val blue_50 = Color(0xFFE3F2FD)
+val blue_100 = Color(0xFFBBDEFB)
+val blue_200 = Color(0xFF90CAF9)
+val blue_300 = Color(0xFF64B5F6)
+val blue_400 = Color(0xFF42A5F5)
+val blue_500 = Color(0xFF2196F3)
+val blue_600 = Color(0xFF1E88E5)
+val blue_700 = Color(0xFF1976D2)
+val blue_800 = Color(0xFF1565C0)
+val blue_900 = Color(0xFF0D47A1)
+val blue_A100 = Color(0xFF82B1FF)
+val blue_A200 = Color(0xFF448AFF)
+val blue_A400 = Color(0xFF2979FF)
+val blue_A700 = Color(0xFF2962FF)
+val blue = blue_500
+
+
+val lightBlue_50 = Color(0xFFE1F5FE)
+val lightBlue_100 = Color(0xFFB3E5FC)
+val lightBlue_200 = Color(0xFF81D4FA)
+val lightBlue_300 = Color(0xFF4FC3F7)
+val lightBlue_400 = Color(0xFF29B6F6)
+val lightBlue_500 = Color(0xFF03A9F4)
+val lightBlue_600 = Color(0xFF039BE5)
+val lightBlue_700 = Color(0xFF0288D1)
+val lightBlue_800 = Color(0xFF0277BD)
+val lightBlue_900 = Color(0xFF01579B)
+val lightBlue_A100 = Color(0xFF80D8FF)
+val lightBlue_A200 = Color(0xFF40C4FF)
+val lightBlue_A400 = Color(0xFF00B0FF)
+val lightBlue_A700 = Color(0xFF0091EA)
+val lightBlue = lightBlue_500
+
+
+val cyan_50 = Color(0xFFE0F7FA)
+val cyan_100 = Color(0xFFB2EBF2)
+val cyan_200 = Color(0xFF80DEEA)
+val cyan_300 = Color(0xFF4DD0E1)
+val cyan_400 = Color(0xFF26C6DA)
+val cyan_500 = Color(0xFF00BCD4)
+val cyan_600 = Color(0xFF00ACC1)
+val cyan_700 = Color(0xFF0097A7)
+val cyan_800 = Color(0xFF00838F)
+val cyan_900 = Color(0xFF006064)
+val cyan_A100 = Color(0xFF84FFFF)
+val cyan_A200 = Color(0xFF18FFFF)
+val cyan_A400 = Color(0xFF00E5FF)
+val cyan_A700 = Color(0xFF00B8D4)
+val cyan = cyan_500
+
+
+val teal_50 = Color(0xFFE0F2F1)
+val teal_100 = Color(0xFFB2DFDB)
+val teal_200 = Color(0xFF80CBC4)
+val teal_300 = Color(0xFF4DB6AC)
+val teal_400 = Color(0xFF26A69A)
+val teal_500 = Color(0xFF009688)
+val teal_600 = Color(0xFF00897B)
+val teal_700 = Color(0xFF00796B)
+val teal_800 = Color(0xFF00695C)
+val teal_900 = Color(0xFF004D40)
+val teal_A100 = Color(0xFFA7FFEB)
+val teal_A200 = Color(0xFF64FFDA)
+val teal_A400 = Color(0xFF1DE9B6)
+val teal_A700 = Color(0xFF00BFA5)
+val teal = teal_500
+
+
+val green_50 = Color(0xFFE8F5E9)
+val green_100 = Color(0xFFC8E6C9)
+val green_200 = Color(0xFFA5D6A7)
+val green_300 = Color(0xFF81C784)
+val green_400 = Color(0xFF66BB6A)
+val green_500 = Color(0xFF4CAF50)
+val green_600 = Color(0xFF43A047)
+val green_700 = Color(0xFF388E3C)
+val green_800 = Color(0xFF2E7D32)
+val green_900 = Color(0xFF1B5E20)
+val green_A100 = Color(0xFFB9F6CA)
+val green_A200 = Color(0xFF69F0AE)
+val green_A400 = Color(0xFF00E676)
+val green_A700 = Color(0xFF00C853)
+val green = green_500
+
+
+val lightGreen_50 = Color(0xFFF1F8E9)
+val lightGreen_100 = Color(0xFFDCEDC8)
+val lightGreen_200 = Color(0xFFC5E1A5)
+val lightGreen_300 = Color(0xFFAED581)
+val lightGreen_400 = Color(0xFF9CCC65)
+val lightGreen_500 = Color(0xFF8BC34A)
+val lightGreen_600 = Color(0xFF7CB342)
+val lightGreen_700 = Color(0xFF689F38)
+val lightGreen_800 = Color(0xFF558B2F)
+val lightGreen_900 = Color(0xFF33691E)
+val lightGreen_A100 = Color(0xFFCCFF90)
+val lightGreen_A200 = Color(0xFFB2FF59)
+val lightGreen_A400 = Color(0xFF76FF03)
+val lightGreen_A700 = Color(0xFF64DD17)
+val lightGreen = lightGreen_500
+
+
+val lime_50 = Color(0xFFF9FBE7)
+val lime_100 = Color(0xFFF0F4C3)
+val lime_200 = Color(0xFFE6EE9C)
+val lime_300 = Color(0xFFDCE775)
+val lime_400 = Color(0xFFD4E157)
+val lime_500 = Color(0xFFCDDC39)
+val lime_600 = Color(0xFFC0CA33)
+val lime_700 = Color(0xFFAFB42B)
+val lime_800 = Color(0xFF9E9D24)
+val lime_900 = Color(0xFF827717)
+val lime_A100 = Color(0xFFF4FF81)
+val lime_A200 = Color(0xFFEEFF41)
+val lime_A400 = Color(0xFFC6FF00)
+val lime_A700 = Color(0xFFAEEA00)
+val lime = lime_500
+
+
+val yellow_50 = Color(0xFFFFFDE7)
+val yellow_100 = Color(0xFFFFF9C4)
+val yellow_200 = Color(0xFFFFF59D)
+val yellow_300 = Color(0xFFFFF176)
+val yellow_400 = Color(0xFFFFEE58)
+val yellow_500 = Color(0xFFFFEB3B)
+val yellow_600 = Color(0xFFFDD835)
+val yellow_700 = Color(0xFFFBC02D)
+val yellow_800 = Color(0xFFF9A825)
+val yellow_900 = Color(0xFFF57F17)
+val yellow_A100 = Color(0xFFFFFF8D)
+val yellow_A200 = Color(0xFFFFFF00)
+val yellow_A400 = Color(0xFFFFEA00)
+val yellow_A700 = Color(0xFFFFD600)
+val yellow = yellow_500
+
+
+val amber_50 = Color(0xFFFFF8E1)
+val amber_100 = Color(0xFFFFECB3)
+val amber_200 = Color(0xFFFFE082)
+val amber_300 = Color(0xFFFFD54F)
+val amber_400 = Color(0xFFFFCA28)
+val amber_500 = Color(0xFFFFC107)
+val amber_600 = Color(0xFFFFB300)
+val amber_700 = Color(0xFFFFA000)
+val amber_800 = Color(0xFFFF8F00)
+val amber_900 = Color(0xFFFF6F00)
+val amber_A100 = Color(0xFFFFE57F)
+val amber_A200 = Color(0xFFFFD740)
+val amber_A400 = Color(0xFFFFC400)
+val amber_A700 = Color(0xFFFFAB00)
+val amber = amber_500
+
+
+val orange_50 = Color(0xFFFFF3E0)
+val orange_100 = Color(0xFFFFE0B2)
+val orange_200 = Color(0xFFFFCC80)
+val orange_300 = Color(0xFFFFB74D)
+val orange_400 = Color(0xFFFFA726)
+val orange_500 = Color(0xFFFF9800)
+val orange_600 = Color(0xFFFB8C00)
+val orange_700 = Color(0xFFF57C00)
+val orange_800 = Color(0xFFEF6C00)
+val orange_900 = Color(0xFFE65100)
+val orange_A100 = Color(0xFFFFD180)
+val orange_A200 = Color(0xFFFFAB40)
+val orange_A400 = Color(0xFFFF9100)
+val orange_A700 = Color(0xFFFF6D00)
+val orange = orange_500
+
+
+val deepOrange_50 = Color(0xFFFBE9E7)
+val deepOrange_100 = Color(0xFFFFCCBC)
+val deepOrange_200 = Color(0xFFFFAB91)
+val deepOrange_300 = Color(0xFFFF8A65)
+val deepOrange_400 = Color(0xFFFF7043)
+val deepOrange_500 = Color(0xFFFF5722)
+val deepOrange_600 = Color(0xFFF4511E)
+val deepOrange_700 = Color(0xFFE64A19)
+val deepOrange_800 = Color(0xFFD84315)
+val deepOrange_900 = Color(0xFFBF360C)
+val deepOrange_A100 = Color(0xFFFF9E80)
+val deepOrange_A200 = Color(0xFFFF6E40)
+val deepOrange_A400 = Color(0xFFFF3D00)
+val deepOrange_A700 = Color(0xFFDD2C00)
+val deepOrange = deepOrange_500
+
+
+val brown_50 = Color(0xFFEFEBE9)
+val brown_100 = Color(0xFFD7CCC8)
+val brown_200 = Color(0xFFBCAAA4)
+val brown_300 = Color(0xFFA1887F)
+val brown_400 = Color(0xFF8D6E63)
+val brown_500 = Color(0xFF795548)
+val brown_600 = Color(0xFF6D4C41)
+val brown_700 = Color(0xFF5D4037)
+val brown_800 = Color(0xFF4E342E)
+val brown_900 = Color(0xFF3E2723)
+val brown = brown_500
+
+
+val grey_50 = Color(0xFFFAFAFA)
+val grey_100 = Color(0xFFF5F5F5)
+val grey_200 = Color(0xFFEEEEEE)
+val grey_300 = Color(0xFFE0E0E0)
+val grey_400 = Color(0xFFBDBDBD)
+val grey_500 = Color(0xFF9E9E9E)
+val grey_600 = Color(0xFF757575)
+val grey_700 = Color(0xFF616161)
+val grey_800 = Color(0xFF424242)
+val grey_900 = Color(0xFF212121)
+val grey = grey_500
+
+
+val blueGrey_50 = Color(0xFFECEFF1)
+val blueGrey_100 = Color(0xFFCFD8DC)
+val blueGrey_200 = Color(0xFFB0BEC5)
+val blueGrey_300 = Color(0xFF90A4AE)
+val blueGrey_400 = Color(0xFF78909C)
+val blueGrey_500 = Color(0xFF607D8B)
+val blueGrey_600 = Color(0xFF546E7A)
+val blueGrey_700 = Color(0xFF455A64)
+val blueGrey_800 = Color(0xFF37474F)
+val blueGrey_900 = Color(0xFF263238)
+val blueGrey = blueGrey_500
+
+
+//val _50 = Color(0xFF)
+//val _100 = Color(0xFF)
+//val _200 = Color(0xFF)
+//val _300 = Color(0xFF)
+//val _400 = Color(0xFF)
+//val _500 = Color(0xFF)
+//val _600 = Color(0xFF)
+//val _700 = Color(0xFF)
+//val _800 = Color(0xFF)
+//val _900 = Color(0xFF)
+//val _A100 = Color(0xFF)
+//val _A200 = Color(0xFF)
+//val _A400 = Color(0xFF)
+//val _A700 = Color(0xFF)
